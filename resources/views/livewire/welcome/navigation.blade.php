36792c4ebbb4a23643c5468f6d4379a5bf1ad772
lau <lau.bannenberg@gmail.com>
2024-05-06 15:41:31 +0200

    Add logout button to welcome page if already logged in

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,3 +1,20 @@
+<?php
+
+use App\Livewire\Actions\Logout;
+use Livewire\Volt\Component;
+
+new class extends Component {
+    /**
+     * Log the current user out of the application.
+     */
+    public function logout(Logout $logout): void
+    {
+        $logout();
+
+        $this->redirect('/', navigate: true);
+    }
+}; ?>
+
 <nav class="-mx-3 flex flex-1 justify-end">
     @auth
         <a
@@ -6,6 +23,13 @@
         >
             Dashboard
         </a>
+
+        <button wire:click="logout"
+                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+        >
+            {{ __('Log Out') }}
+        </button>
+
     @else
         <a
             href="{{ route('login') }}"
