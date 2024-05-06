949a4a9c162195a674cec671dff77671b6757f35
lau <lau.bannenberg@gmail.com>
2024-05-06 16:55:04 +0200

    Include Pulse in navigation

--- resources/views/vendor/pulse/dashboard.blade.php
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -1,10 +1,12 @@
 <x-pulse full-width>
     <a href="{{route('dashboard')}}" class="text-md sm:text-lg text-gray-700 dark:text-gray-300 font-medium">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" style="display: inline">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
         </svg>
         <span>{{__('Dashboard')}}</span>
     </a>
+
+
 
     <livewire:pulse.servers cols="full" />
 
