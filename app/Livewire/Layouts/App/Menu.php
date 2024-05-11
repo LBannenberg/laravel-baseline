8c83c7795cbc911ff617ec0d5311f8df489b06e1
lau <lau.bannenberg@gmail.com>
2024-05-11 23:42:54 +0200

    pint

--- app/Livewire/Layouts/App/Menu.php
+++ app/Livewire/Layouts/App/Menu.php
@@ -18,20 +18,20 @@ class Menu extends Component
     public function mount(string $menuType): void
     {
         $this->menuType = $menuType;
-        $items = config('menu.app');;
+        $items = config('menu.app');
         $items = $this->applyGates($items);
         $items = $this->applyTranslations($items);
         $items = $this->applyRoutes($items);
         $this->items = $items;
     }
 
-    public function render()
+    public function render(): \Illuminate\Contracts\View\View
     {
-        return $this->menuType == 'sidebar'
-            ? view('livewire.layouts.app.menu-sidebar')
-            : view('livewire.layouts.app.menu-mobile');
+        return match ($this->menuType) {
+            'sidebar' => view('livewire.layouts.app.menu-sidebar'),
+            'mobile' => view('livewire.layouts.app.menu-mobile')
+        };
     }
-
 
     /**
      * Log the current user out of the application.
@@ -43,42 +43,45 @@ class Menu extends Component
         $this->redirect('/', navigate: true);
     }
 
-
     private function applyGates(array $items): array
     {
         $result = [];
-        foreach($items as $item) {
-            if(!isset($item['roles'])) {
+        foreach ($items as $item) {
+            if (! isset($item['roles'])) {
                 $result[] = $item;
+
                 continue;
             }
 
-            if(in_array(auth()->user()->role, $item['roles'])) {
+            if (in_array(auth()->user()?->role, $item['roles'])) {
                 $result[] = $item;
             }
         }
+
         return $result;
     }
 
-
     private function applyTranslations(array $items): array
     {
-        foreach($items as &$item) {
+        foreach ($items as &$item) {
             $item['label'] = __($item['label']);
         }
+
         return $items;
     }
 
     private function applyRoutes(array $items): array
     {
-        foreach($items as &$item) {
-            if(!isset($item['route'])) {
+        foreach ($items as &$item) {
+            if (! isset($item['route'])) {
                 $item['active'] = false;
+
                 continue;
             }
             $item['active'] = request()->routeIs($item['route']);
             $item['route'] = route($item['route']);
         }
+
         return $items;
     }
 }
