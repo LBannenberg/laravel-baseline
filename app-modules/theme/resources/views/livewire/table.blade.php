fabd344f2b3f4fe7873bce9171d892e5028f9894
lau <lau.bannenberg@gmail.com>
2024-06-15 00:32:26 +0200

    Remove "relative" from table, it messes with the mobile hamburger menu

--- app-modules/theme/resources/views/livewire/table.blade.php
+++ app-modules/theme/resources/views/livewire/table.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="relative overflow-x-auto shadow-md rounded-lg">
+    <div class="overflow-x-auto shadow-md rounded-lg">
         <table class="w-full text-sm text-left text-gray-500">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
