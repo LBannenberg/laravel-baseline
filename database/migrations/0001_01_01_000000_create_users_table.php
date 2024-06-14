8d46914f2c676528d6505ddf7af63afbb1f37990
lau <lau.bannenberg@gmail.com>
2024-06-14 21:08:40 +0200

    Make users soft-deletable, move role to core migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('role', length: 32)->default('User');
+            $table->softDeletes();
             $table->timestamps();
         });
 
